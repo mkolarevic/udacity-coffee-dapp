638134994e5526dc750ef5aa3e371e0d2eafbf4b
Matej Kolarević <matej.kolarevic@barrage.net>
2022-12-05 09:04:49 +0100

    Add sepolia testnet config

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -1,14 +1,31 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = "60d3dffa0e5546e0981f6161d0f163fa";
+
+const fs = require('fs');
+const mnemonic = "apart fade this catch illness eagle melt panther trap split spot opera";
+
 module.exports = {
+
+
   networks: {
+
     development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*" // Match any network id
-    }
+      network_id: "*"       // Match any network
+    },
+
+    sepolia: {
+      provider: () => new HDWalletProvider(mnemonic, `https://sepolia.infura.io/v3/${infuraKey}`),
+        network_id: 11155111,       // sepolia's id
+        gas: 4500000,
+        gasPrice: 10000000000
+    },
   },
+
   compilers: {
     solc: {
       version: "^0.5.0",
     }
   }
-};
+}
